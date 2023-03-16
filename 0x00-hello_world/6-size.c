f02bafc9c9f293c70b67ba0d9737bdd96190dc93
Tobit Prince <68125101+tobitprince@users.noreply.github.com>
2023-03-16 18:53:37 +0300

    Create 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main (void)
+{
+  printf("The size of char is : %d/n",size of (char));
+  printf("The size of int is : %d/n", size of (int));
+  printf("The size of long int is : %d/n",size of (long int));
+  printf("The size of long long int is: %d",size of (long long int));
+  printf("The size of float is : %d",size of (float));
+  return (0);
+}
