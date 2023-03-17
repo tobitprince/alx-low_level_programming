b3e224d9b59c70d27ee0aa7173d7c55f7d6796b3
Tobit Prince <68125101+tobitprince@users.noreply.github.com>
2023-03-17 22:47:51 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	char alp[26] = "abcdfghijklmnoprstuvwxyz";
+	char alp[24] = "abcdfghijklmnoprstuvwxyz";
 	int i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 0; i < 24; i++)
 	{
 		putchar(alp[i]);
 	}
