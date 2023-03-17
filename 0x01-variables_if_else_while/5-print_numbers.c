6f9373d7c1bd65f1d864bca9e2bd7238ca2b22d4
Tobit Prince <68125101+tobitprince@users.noreply.github.com>
2023-03-17 22:53:26 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet.
+ * main - Prints numbers from 0 to 9.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int num=[0,1,2,3,4,5,6,7,8,9];
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar(num[i]);
+		printf("%d", i);
 	}
 	putchar('\n');
 	return (0);
+}
