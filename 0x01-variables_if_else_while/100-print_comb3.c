74eea4b828f5a06817b6b0c977ffff14c2fc5c64
Tobit Prince <68125101+tobitprince@users.noreply.github.com>
2023-03-18 14:18:10 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,11 +2,14 @@
 
 int main() {
     int i, j;
-    for (i = 0; i < 9; i++) {
-        for (j = i + 1; j < 10; j++) {
+    for (i = 0; i < 9; i++)
+    {
+        for (j = i + 1; j < 10; j++)
+        {
             putchar(i + '0');
             putchar(j + '0');
-            if (i != 8 || j != 9) {
+            if (i != 8 || j != 9)
+            {
                 putchar(44);
                 putchar(32);
             }
