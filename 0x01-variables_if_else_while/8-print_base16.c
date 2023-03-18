ace84757341bbf34ce581d3fcb7bff300070ab07
Tobit Prince <68125101+tobitprince@users.noreply.github.com>
2023-03-18 13:46:45 +0300

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char hex_digits[] = "0123456789abcdef";
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		putchar(hex_digits[i]);
+	}
+	putchar('\n');
+	return (0);
+}
