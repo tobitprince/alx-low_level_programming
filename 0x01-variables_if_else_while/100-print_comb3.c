339321ef23b2178f57826883a6ad06873211aa5c
Tobit Prince <68125101+tobitprince@users.noreply.github.com>
2023-03-20 13:08:29 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers from 0 to 9.
- *
- * Return: Always 0 (Success)
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
  */
-
 int main(void)
 {
-        int i, j;
-        for (i = 0; i < 9; i++)
-        {
-        for (j = i + 1; j < 10; j++)
-        {
-        putchar(i + '0');
-        putchar(j + '0');
-        if (i != 8 || j != 9)
-        {
-        putchar(44);
-        putchar(32);
-        }
-        }
-    }
-    putchar('\n');
-    return 0;
-}
+	int n, m;
 
+	for (n = 48; n <= 56; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
