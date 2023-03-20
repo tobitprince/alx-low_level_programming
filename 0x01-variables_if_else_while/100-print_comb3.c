ef34a9f7135a74088bb941df87a0df9691e2ec6b
Tobit Prince <68125101+tobitprince@users.noreply.github.com>
2023-03-20 13:07:03 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,20 +6,20 @@
  * Return: Always 0 (Success)
  */
 
-int main()
+int main(void)
 {
-    int i, j;
-    for (i = 0; i < 9; i++)
-    {
+        int i, j;
+        for (i = 0; i < 9; i++)
+        {
         for (j = i + 1; j < 10; j++)
         {
-            putchar(i + '0');
-            putchar(j + '0');
-            if (i != 8 || j != 9)
-            {
-                putchar(44);
-                putchar(32);
-            }
+        putchar(i + '0');
+        putchar(j + '0');
+        if (i != 8 || j != 9)
+        {
+        putchar(44);
+        putchar(32);
+        }
         }
     }
     putchar('\n');
