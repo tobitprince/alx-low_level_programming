46f45b86d294d9f7a7841e72af2e641d6466af4d
Tobit Prince <68125101+tobitprince@users.noreply.github.com>
2023-03-21 21:47:50 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,14 +9,14 @@
 
 int main(void)
 {
-  char put[8]="_putchar";
-  int i;
+char put[8]="_putchar";
+int i;
   
-  for (i=0;i<8;i++)
-  {
-    putchar(put[i]);
-  }
+for (i=0;i<8;i++)
+{
+putchar(put[i]);
+}
   
-  putchar('\n');
-  return (0);
+putchar('\n');
+return (0);
 }
