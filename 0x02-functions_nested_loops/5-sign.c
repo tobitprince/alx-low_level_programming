e37fc5de8b5074ab453c74968536941a6e885c6b
Tobit Prince <68125101+tobitprince@users.noreply.github.com>
2023-03-22 20:19:30 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,25 +3,25 @@
 /**
 *print_sign - prints the sign
 *Return: 1
-*
+*@n: checks
 *
 */
 
 int print_sign(int n)
 {
-  if (n > 0)
-  {
-    _putchar('+');
-    return (1);
-  }
-  else if (n == 0)
-  {
-    _putchar('0');
-    return (0);
-  }
-  else
-  {
-    _putchar('-');
-    return (-1);
-  }
+    if (n > 0)
+    {
+        _putchar('+');
+        return (1);
+    }
+    else if (n == 0)
+    {
+        _putchar('0');
+        return (0);
+    }
+    else
+    {
+        _putchar('-');
+        return (-1);
+    }
 }
